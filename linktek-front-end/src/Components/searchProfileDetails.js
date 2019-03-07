256436ae83831d3bbc1322f872aae81f04e682cd
romain.pawlaczyk <romain.pawlaczyk@epitech.eu>
2019-03-07 21:55:41 +0100

    add card modal for profile search

--- linktek-front-end/src/Components/searchProfileDetails.js
+++ linktek-front-end/src/Components/searchProfileDetails.js
@@ -1,39 +1,20 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import CloseIcon from '@material-ui/icons/Close';
-import Slide from '@material-ui/core/Slide';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Picture1 from '../Images/profilePicture1.png';
 import Picture2 from '../Images/profilePicture2.png';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Profile from './searchProfileModuleDetails.js';
-
-const useStyles = makeStyles({
-  appBar: {
-    position: 'relative',
-  },
-  flex: {
-    flex: 1,
-  },
-});
-
-function Transition(props) {
-  return <Slide direction="up" {...props} />;
-}
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
 
 function SearchProfileDetails(props) {
-  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
 
   function handleClickOpen() {
@@ -67,25 +48,21 @@ function SearchProfileDetails(props) {
         </ListItemSecondaryAction>
       </ListItem>
 
-      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
-        <AppBar className={classes.appBar}>
-          <Toolbar>
-            <Typography variant="h6" color="inherit" className={classes.flex}>
-              Jean Valjean Profile
-            </Typography>
-            <Button color="inherit" onClick={handleClose}>
-              <CloseIcon />
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <Profile
-          age={33}
-          userName={"Jean Valjean"}
-          company={"Les Tenardiers"}
-          job={"Bagnard"}
-          country={"France"}/>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          <Profile
+            age={33}
+            userName={"Jean Valjean"}
+            company={"Les Tenardiers"}
+            job={"Bagnard"}
+            country={"France"}/>
+        </DialogContent>
       </Dialog>
-
     </div>
   );
 }
